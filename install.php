31f5607a0717cbbb195eb6b5f99a248576951e2b
Philippe Lindheimer <philippe.lindheimer@schmoozecom.com>
2011-01-18 20:53:25 +0000

    get rid of UINT, adds validation and method to check results of last update re #4740, this checking may momentarily break stuff as it has not been tested with setting values, about to do that so fixes to come shortly

--- install.php
+++ install.php
@@ -23,34 +23,37 @@ $freepbx_conf =& freepbx_conf::create();
   // 
   $set['value'] = 6;
   $set['defaultval'] =& $set['value'];
+  $set['options'] = array(6,10,20,30,45,60,120,300,600);
   $set['readonly'] = 0;
   $set['hidden'] = 0;
   $set['level'] = 0;
   $set['module'] = 'dashboard';
   $set['category'] = 'GUI Behavior';
-  $set['emptyok'] = 1;
+  $set['emptyok'] = 0;
   $set['description'] = 'Update rate in seconds of all sections of the System Status panel except the Info box.';
-  $set['type'] = CONF_TYPE_UINT;
+  $set['type'] = CONF_TYPE_SELECT;
   $freepbx_conf->define_conf_setting('DASHBOARD_STATS_UPDATE_TIME',$set);
 
   // DASHBOARD_INFO_UPDATE_TIME
   //
   $set['value'] = 30;
   $set['defaultval'] =& $set['value'];
+  $set['options'] = array(15,30,60,120,300,600);
   $set['readonly'] = 0;
   $set['hidden'] = 0;
   $set['level'] = 0;
   $set['module'] = 'dashboard';
   $set['category'] = 'GUI Behavior';
-  $set['emptyok'] = 1;
+  $set['emptyok'] = 0;
   $set['description'] = 'Update rate in seconds of the Info section of the System Status panel.';
-  $set['type'] = CONF_TYPE_UINT;
+  $set['type'] = CONF_TYPE_SELECT;
   $freepbx_conf->define_conf_setting('DASHBOARD_INFO_UPDATE_TIME',$set);
 
   // SSHPORT
   //
   $set['value'] = '';
   $set['defaultval'] =& $set['value'];
+  $set['options'] = array(1,65535);
   $set['readonly'] = 0;
   $set['hidden'] = 0;
   $set['level'] = 2;
@@ -58,13 +61,14 @@ $freepbx_conf =& freepbx_conf::create();
   $set['category'] = 'System Setup';
   $set['emptyok'] = 1;
   $set['description'] = 'SSH port number configured on your system if not using the default port 22, this allows the dashboard monitoring to watch the poper port.';
-  $set['type'] = CONF_TYPE_UINT;
+  $set['type'] = CONF_TYPE_INT;
   $freepbx_conf->define_conf_setting('SSHPORT',$set);
 
   // MAXCALLS
   //
   $set['value'] = '';
   $set['defaultval'] =& $set['value'];
+  $set['options'] = array(0,3000);
   $set['readonly'] = 0;
   $set['hidden'] = 0;
   $set['level'] = 2;
@@ -72,7 +76,7 @@ $freepbx_conf =& freepbx_conf::create();
   $set['category'] = 'GUI Behavior';
   $set['emptyok'] = 1;
   $set['description'] = 'Use this to pre-set the scale for maximum calls on the Dashboard display. If not set, the the scale is dynamically sized based on the active calls on the system.';
-  $set['type'] = CONF_TYPE_UINT;
+  $set['type'] = CONF_TYPE_INT;
   $freepbx_conf->define_conf_setting('MAXCALLS',$set);
 
   $freepbx_conf->commit_conf_settings();
