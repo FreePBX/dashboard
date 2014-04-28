79351e37f53624fdda6650be6b71571110e57edc
Anthony Lewis <tony.lewis@schmoozecom.com>
2014-04-28 14:50:57 -0500

    new license

--- class.error.inc.php
+++ class.error.inc.php
@@ -2,8 +2,7 @@
 if (!defined('FREEPBX_IS_AUTH')) { die('No direct script access allowed'); }
 //	License for all code of this FreePBX module can be found in the license file inside the module directory
 //	Copyright 2013 Schmooze Com Inc.
-//  Copyright 2006 phpSysInfo
-
+//
 class Error {
 
 	// Array which holds the error messages
--- functions.inc.php
+++ functions.inc.php
@@ -2,6 +2,5 @@
 if (!defined('FREEPBX_IS_AUTH')) { die('No direct script access allowed'); }
 //	License for all code of this FreePBX module can be found in the license file inside the module directory
 //	Copyright 2013 Schmooze Com Inc.
-//  Copyright (C) 2006 Philippe Lindheimer
-
+//
 ?>
