d4b06f774f765f82cdddeb8fa0fde271dfd21010
Andrew Nagy <andrew.nagy@the159.com>
2014-06-18 13:37:09 -0400

    More this setting into this module

---
+++ install.php
@@ -0,0 +1,17 @@
+<?php
+$set = array();
+// RSSFEEDS
+$set['category'] = 'System Setup';
+$set['value'] = "http://www.freepbx.org/rss.xml\nhttp://feeds.feedburner.com/InsideTheAsterisk";
+$set['defaultval'] = $set['value'];
+$set['name'] = 'RSS Feeds';
+$set['description'] = 'RSS Feeds that are displayed in UCP and Dashboard. Separate each feed by a new line';
+$set['hidden'] = 0;
+$set['emptyok'] = 0;
+$set['readonly'] = 0;
+$set['level'] = 0;
+$set['options'] = '';
+$set['module'] = '';
+$set['type'] = CONF_TYPE_TEXTAREA;
+FreePBX::Config()->define_conf_setting('RSSFEEDS',$set);
+FreePBX::Config()->commit_conf_settings();
