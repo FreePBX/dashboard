c7807077e811a6a0c30522de13354c6932da56ee
Andrew Nagy <andrew.nagy@the159.com>
2015-02-27 20:49:58 -0500

    make rss feeds more resilient

--- views/sections/blog.php
+++ views/sections/blog.php
@@ -1,15 +1,20 @@
 <ul>
-<?php 
+<?php
+$c = 0;
 foreach($items as $item) {
-	if ($item['description']) {
-		$desc = $item['description'];
+	if($c > $limit) {
+		break;
+	}
+	if ((string)$item->description) {
+		$desc = (string)$item->description;
 		$tooltip = substr(strip_tags($desc,'<br>'),0,200)."...";
 	} else {
 		$tooltip = "";
 	}
-	$href = $item['link'];
-	$title = $item['title'];
+	$href = (string)$item->link;
+	$title = (string)$item->title;
 	print "<li><small><a data-toggle='tooltip' title='$tooltip' href='$href' target='_blank'>$title</a></small></li>\n";
+	$c++;
 }
 ?>
 </ul>
