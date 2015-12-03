fea96dd454ee805ae5bc5a511eefb53cdc3e5e29
Andrew Nagy <andrew.nagy@the159.com>
2015-12-03 11:58:31 -0800

    FREEPBX-10834

--- sections/Blogs.class.php
+++ sections/Blogs.class.php
@@ -9,85 +9,93 @@ use PicoFeed\Reader\Reader;
 
 class Blogs {
 	public $rawname = 'Blogs';
-	private $urls = array();
-	private $fastFeed = null;
 
-	public function __construct() {
-		$reader = new Reader;
-
+	public function getSections($order) {
 		$feeds = \FreePBX::Config()->get('RSSFEEDS');
 		$feeds = str_replace("\r","",$feeds);
+		$blogs = array();
 		if(!empty($feeds)) {
 			$feeds = explode("\n",$feeds);
 			$i = 0;
-			$this->urls = array();
-			$d = \FreePBX::Dashboard();
-
+			$urls = array();
 			foreach($feeds as $feed) {
-
-				$etag = $d->getConfig($feed, "etag");
-				$last_modified = $d->getConfig($feed, "last_modified");
-				try {
-					$resource = $reader->download($feed, $last_modified, $etag);
-					if ($resource->isModified()) {
-
-						$parser = $reader->getParser(
-							$resource->getUrl(),
-							$resource->getContent(),
-							$resource->getEncoding()
+				$data = $this->getFeed($feed);
+				if(!empty($data)) {
+					$title = $data->title;
+					if(!empty($title)) {
+						$blogs[] = array(
+							"title" => sprintf(_('%s Feed'),$title),
+							"group" => _("Blogs"),
+							"width" => "550px",
+							"order" => isset($order[$feed]) ? $order[$feed] : '100',
+							"section" => $i
 						);
-
-						$content = $parser->execute();
-						$etag = $resource->getEtag();
-						$last_modified = $resource->getLastModified();
-
-						$d->setConfig($feed, $content, "content");
-						$d->setConfig($feed, $etag, "etag");
-						$d->setConfig($feed, $last_modified, "last_modified");
 					} else {
-						$content = $d->getConfig($feed, "content");
-					}
-					$this->urls['rss-'.$i] = $content;
-					$i++;
-				}	catch (\PicoFeed\PicoFeedException $e) {
-					$content = $d->getConfig($feed, "content");
-					if(!empty($content)) {
-						$this->urls['rss-'.$i] = $content;
-						$i++;
+						$blogs[] = array(
+							"title" => sprintf(_('%s Feed'),$feed),
+							"group" => _("Blogs"),
+							"width" => "500px",
+							"order" => isset($order[$name]) ? $order[$feed] : '100',
+							"section" => $i
+						);
 					}
 				}
-			}
-		}
-	}
-
-	public function getSections($order) {
-		$blogs = array();
-		foreach($this->urls as $name => $url) {
-			$feed = $this->urls[$name];
-			$title = $feed->title;
-			if(!empty($title)) {
-				$blogs[] = array(
-					"title" => $title . " " . _('Feed'),
-					"group" => _("Blogs"),
-					"width" => "550px",
-					"order" => isset($order[$name]) ? $order[$name] : '100',
-					"section" => $name
-				);
-			} else {
-				$blogs[] = array(
-					"title" => $name . " " . _('Feed'),
-					"group" => _("Blogs"),
-					"width" => "500px",
-					"order" => isset($order[$name]) ? $order[$name] : '100',
-					"section" => $name
-				);
+				$i++;
 			}
 		}
 		return $blogs;
 	}
 
 	public function getContent($section) {
-		$feed = $this->urls[$section];
+		$feeds = \FreePBX::Config()->get('RSSFEEDS');
+		$feeds = str_replace("\r","",$feeds);
+		if(empty($feeds)) {
+			return '';
+		}
+		$feeds = explode("\n",$feeds);
+		if(empty($feeds[$section])) {
+			return '';
+		}
+		$feed = $this->getFeed($feeds[$section]);
+		if(empty($feed)) {
+			return '';
+		}
 		return load_view(dirname(__DIR__).'/views/sections/blog.php',array("items" => $feed->items, "limit" => 5));
+	}
+
+	/**
+	 * Get the feed from cache or retrieve it
+	 * @param  string $feed The feed URL
+	 * @return object       Reader object
+	 */
+	private function getFeed($feed) {
+		$reader = new Reader;
+		$d = \FreePBX::Dashboard();
+		$etag = $d->getConfig($feed, "etag");
+		$last_modified = $d->getConfig($feed, "last_modified");
+		try {
+			$resource = $reader->download($feed, $last_modified, $etag);
+			if ($resource->isModified()) {
+
+				$parser = $reader->getParser(
+					$resource->getUrl(),
+					$resource->getContent(),
+					$resource->getEncoding()
+				);
+
+				$content = $parser->execute();
+				$etag = $resource->getEtag();
+				$last_modified = $resource->getLastModified();
+
+				$d->setConfig($feed, $content, "content");
+				$d->setConfig($feed, $etag, "etag");
+				$d->setConfig($feed, $last_modified, "last_modified");
+			} else {
+				$content = $d->getConfig($feed, "content");
+			}
+		}	catch (\PicoFeed\PicoFeedException $e) {
+			$content = $d->getConfig($feed, "content");
+		}
+		return $content;
 	}
 }
