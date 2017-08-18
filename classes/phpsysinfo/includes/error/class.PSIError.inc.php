d0d4e50f9fcda5f211ec81429ccfee113708b7b7
Andrew Nagy <andrew.nagy@the159.com>
2017-08-18 16:39:22 -0400

    remove comment

--- classes/phpsysinfo/includes/error/class.PSIError.inc.php
+++ classes/phpsysinfo/includes/error/class.PSIError.inc.php
@@ -63,7 +63,6 @@ class PSIError
      */
     public static function singleton()
     {
-				dbug("yes");
         if (!isset(self::$_instance)) {
             $c = __CLASS__;
             self::$_instance = new $c;
